d235ea4ae4d7cd738296b0e63fbb195383e7383f
Atif Riaz <anjum.atif900@gmail.com>
2022-07-22 06:21:02 -0400

    assignment 1

---
+++ Calculator_Application/Classes/iCalculate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator_Application
+{
+    public interface iCalculate
+    {
+        double Add(double num1, double num2);
+        double Subtract(double num1, double num2);
+        double Multiplication(double num1, double num2);
+        double Division(double num1, double num2);
+    }
+}
---
+++ Calculator_Application/script/Validation.js
@@ -0,0 +1,14 @@
+﻿function Validate() {
+    let value1 = document.querySelector("#num1").value;
+    let value2 = document.querySelector("#num2").value;
+    let operation = document.querySelector("#operation").value;
+
+    if (isNaN(value1) == true) { alert("X should be number"); return false; }
+    if (isNaN(value2) == true) { alert("Y should be number"); return false; }
+    if (value1 === "" || value1 === null) { alert("X is not entered"); return false; };
+    if (value2 === "" || value2 === null) { alert("Y is not entered"); return false; };
+    if (operation == "/" && value2==0){alert("Undefined error Y is zero"); return false;};
+
+    return true;
+
+}
