ed07e7211a24bfa47b9eb1a471ab05596953efcb
Atif Riaz <anjum.atif900@gmail.com>
2022-07-25 00:53:14 -0400

    Assignment 1 final version

---
+++ Calculator_Application/Style and Script/pag1.js
@@ -0,0 +1,15 @@
+﻿function check() {
+    console.log("Hello World")
+    var num1 = document.getElementById("x").value;
+    var num2 = document.getElementById("y").value;
+    var oper = document.getElementById("Oper").value;
+
+    if (isNaN(num1) == true) { alert("Number 1 Should be a Number!"); return false };
+    if (isNaN(num2) == true) { alert("Number 2 Should be a Number!"); return false };
+    if (num1 === "" || num1 === null) { alert("Number 1 is not entered"); return false };
+    if (num2 === "" || num2 === null) { alert("Number 2 is not entered"); return false };
+    if (oper === "/" && num2 === "0") { alert("Answer is Undefine"); return false };
+
+
+    return true;
+}
