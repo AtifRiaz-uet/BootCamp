42d49a4fa01dd8d9eac7838afd54bad6d54f4cb1
Atif Riaz <anjum.atif900@gmail.com>
2022-07-26 01:56:08 -0400

    Assignment3

--- CarsWebApp/Properties/AssemblyInfo.cs
+++ CarsWebApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Student_webApp")]
+[assembly: AssemblyTitle("CarsWebApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Student_webApp")]
+[assembly: AssemblyProduct("CarsWebApp")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6928af16-1c20-47ac-b944-3b6bf0da47c7")]
+[assembly: Guid("0e468997-8382-4029-9450-193204ef8316")]
 
 // Version information for an assembly consists of the following four values:
 //
